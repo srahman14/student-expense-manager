bdd9c0157e098b3981b6647b31a4ebf21ad9c7c5
srahman14 <s.rahman14@hotmail.com>
2025-11-27 02:39:57 +0000

    main entry point of application

---
+++ src/ExpenseManagerApp.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ExpenseManagerApp {
+    public static void main(String[] args) {
+        ExpenseManager manager = new ExpenseManager();
+        manager.startMenu();
+    }
+}
+
